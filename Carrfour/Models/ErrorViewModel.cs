85348990e62acd3d4fc9e331ec23ec0e043b4f1b
eman <eman@gmail.com>
2023-07-12 21:37:10 +0300

    Add project files.

---
+++ Carrfour/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Carrfour.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
